7f49e6b9a927ffa5e95d2f695e8cf58d230b4e9f
Fred Guth <fredguth@fredguth.com>
2024-07-08 10:28:31 -0300

    alpha release

--- fastduck/core.py
+++ fastduck/core.py
@@ -3,8 +3,7 @@
 # %% ../nbs/00_core.ipynb 1
 from __future__ import annotations
 import duckdb
-from duckdb import DuckDBPyConnection
-from duckdb import DuckDBPyRelation
+from duckdb import DuckDBPyConnection, DuckDBPyRelation
 from typing import List, Dict, Optional, Union, Any, Tuple, Set
 from fastcore.all import store_attr, patch, L
 import numpy as np
@@ -12,11 +11,19 @@ import pandas as pd
 from dataclasses import field, make_dataclass
 from fastcore.xtras import hl_md, dataclass_src
 from functools import wraps, partial
+from pathlib import Path
+from IPython.display import Markdown
 
 # %% auto 0
-__all__ = ['props', 'convertTypes', 'clean', 'noop', 'identity', 'custom_dir', 'create_patch_property', 'create_prop']
+__all__ = ['props', 'database', 'convertTypes', 'clean', 'custom_dir', 'create_patch_property', 'create_prop', 'noop', 'identity']
 
-# %% ../nbs/00_core.ipynb 12
+# %% ../nbs/00_core.ipynb 13
+@wraps(duckdb.connect)
+def database(*args, **kwargs):
+    db = duckdb.connect(*args, **kwargs)
+    return db
+
+# %% ../nbs/00_core.ipynb 16
 def _current(self: DuckDBPyConnection): return self.sql('select current_catalog, current_schema').fetchone()
 @patch(as_prop=True)
 def catalog(self: DuckDBPyConnection): return _current(self)[0]
@@ -24,8 +31,8 @@ def catalog(self: DuckDBPyConnection): return _current(self)[0]
 @patch(as_prop=True)
 def schema(self: DuckDBPyConnection): return _current(self)[1]
 
-@patch(as_prop=True) # alias for alias
-def name(self:DuckDBPyRelation): return self.alias
+@patch(as_prop=True) # just the name part in the alias
+def name(self:DuckDBPyRelation): return self.alias.split('.')[-1]
 
 @patch
 def __getitem__(self:DuckDBPyRelation, idxs) -> DuckDBPyRelation: # selecting by passing a list of column names
@@ -34,7 +41,10 @@ def __getitem__(self:DuckDBPyRelation, idxs) -> DuckDBPyRelation: # selecting by
 def to_recs(self:DuckDBPyRelation) -> List[Dict[str, Any]]:
     '''The relation as a list of records'''
     return self.df().to_dict(orient='records')
-
+@patch 
+def to_list(self:DuckDBPyRelation) -> List[List]:
+    '''The relation as a list'''
+    return [list(r.values()) if len(r.values())>1 else list(r.values())[0] for r in self.to_recs() ]
 @patch 
 def q(self:DuckDBPyConnection, *args, **kwargs) -> List[Dict[str, Any]]:
     '''Run a query and return the result as a list of records'''
@@ -42,17 +52,26 @@ def q(self:DuckDBPyConnection, *args, **kwargs) -> List[Dict[str, Any]]:
 
 
 
-# %% ../nbs/00_core.ipynb 16
-@patch
-def _tables(self: DuckDBPyConnection, catalog:str=None) -> DuckDBPyRelation:
-    '''Returns a dictionary of tables in the database'''
-    q = f"from {catalog or self.catalog}.information_schema.tables"
-    s = f"'{catalog or self.catalog}' as catalog, table_schema as schema, table_name as name, table_type as type, table_comment as comment"
-    return self.sql(q).distinct().select(s)
-@patch(as_prop=True)
-def tables(self: DuckDBPyConnection) -> DuckDBPyRelation: return self._tables()
-
 # %% ../nbs/00_core.ipynb 20
+@patch(as_prop=True)
+def tables(self: DuckDBPyConnection) -> DuckDBPyRelation:
+    '''Returns a dictionary of tables in the database'''
+    base = "select distinct database_name as catalog, schema_name as schema"
+    tbls = "table_name as name, 'BASE TABLE' as type, comment from duckdb_tables()"
+    vws = "view_name as name, 'VIEW' as type, comment from duckdb_views()"
+    fltr = "where internal=False"
+    return self.sql(f"{base}, {tbls} {fltr} union all {base}, {vws} {fltr}")
+
+@patch(as_prop=True)
+def views(self: DuckDBPyConnection) -> DuckDBPyRelation:
+    '''Returns a dictionary of views in the database'''
+    return self.tables.filter(f"type =='VIEW' and catalog='{self.catalog}' and schema = '{self.schema}'")
+@patch(as_prop=True)
+def base_tables(self: DuckDBPyConnection) -> DuckDBPyRelation:
+    '''Returns a dictionary of views in the database'''
+    return self.tables.filter(f"type =='BASE TABLE' and catalog='{self.catalog}' and schema = '{self.schema}'")
+
+# %% ../nbs/00_core.ipynb 24
 @patch
 def datamodel(self: DuckDBPyConnection, table_name:str) ->List[Dict]:
     ''' Returns the data model of a table or view. 
@@ -62,7 +81,7 @@ def datamodel(self: DuckDBPyConnection, table_name:str) ->List[Dict]:
     return [{'name': r[1], 'type': r[2], 'nullable': not r[3], 'default': r[4], 'pk': r[5]} 
             for r in self.sql(f"PRAGMA table_info='{table_name}'").fetchall()]
 
-# %% ../nbs/00_core.ipynb 22
+# %% ../nbs/00_core.ipynb 26
 from dataclasses import field, make_dataclass
 def convertTypes(s:str)->type:
     ''' Convert DuckDB types to Python and Numpy types'''
@@ -84,7 +103,8 @@ def convertTypes(s:str)->type:
         'USMALLINT': np.uint16,
         'UINTEGER': np.uint32,
         'UBIGINT': np.uint64,
-        'UTINYINT': np.uint8
+        'UTINYINT': np.uint8,
+        'TIMESTAMP': np.timedelta64
     }
     if s in d: return d[s]
     if s[:7]=='DECIMAL': return float
@@ -109,44 +129,7 @@ def dataclass(self: DuckDBPyConnection,
     fields = [(clean(pref+f['name']+suf), convertTypes(f['type']) if not f['nullable'] else convertTypes(f['type'])|None , field(default=f['default'])) for f in fields]
     return make_dataclass(table_name, fields)
 
-# %% ../nbs/00_core.ipynb 27
-def noop(*args, **kwargs): return None
-def identity(x): return x
-
-
-class _Getter: 
-    """ A Getter utility check https://github.com/AnswerDotAI/fastlite """
-    def __init__(self, name:str='', type:str='', dir:List=[], get=noop): store_attr()    
-    def __dir__(self): return self.dir
-    def __str__(self): return ", ".join(dir(self))
-    def __repr__(self): return f"{self.type}s: {str(self)}"
-    def __contains__(self, s:str): return s in dir(self)
-    def __getitem__(self, k): return self.get(k)
-    def __getattr__(self, k):
-        if k[0]!='_': return self.get(k)
-        else: raise AttributeError 
-
-@patch(as_prop=True) # tables
-def t(self:DuckDBPyConnection): 
-    '''Autocomplete functonality for tables'''
-    return _Getter('Tables', 'Table', [r[0] for r in self.base_tables.select('name').fetchall()], self.table)
-@patch(as_prop=True) # views
-def v(self:DuckDBPyConnection): 
-    '''Autocomplete functonality for views'''
-    return _Getter('Views', 'View', [r[0] for r in self.views.select('name').fetchall()], self.table)
-@patch(as_prop=True) # functions
-def fns(self:DuckDBPyConnection): raise NotImplementedError
-# def fns(self:DuckDBPyConnection): return _Getter(self, f"SELECT function_name FROM duckdb_functions() WHERE schema_name = '{self.schema}' and internal = False")
-
-@patch(as_prop=True) # secrets
-def shh(self:DuckDBPyConnection): raise NotImplementedError
-# def shh(self:DuckDBPyConnection): return _Getter(self, f"SELECT name FROM duckdb_secrets()")
-
-@patch
-def __repr__(self:DuckDBPyConnection): return f'{self.__class__.__name__} ({self.catalog}::{self.schema})'
-
-
-# %% ../nbs/00_core.ipynb 33
+# %% ../nbs/00_core.ipynb 32
 _saved = {}
 
 @patch
@@ -161,7 +144,7 @@ def _get(self:DuckDBPyRelation, key):
     k = str(hash(self))+'_'+key
     return _saved[k] if k in _saved else None
 
-def custom_dir(c, add): return dir(type(c)) + list(c.__dict__.keys()) + add
+def custom_dir(c, add): return dir(type(c)) + list(c.__dict__.keys()) if hasattr(c, '__dict__') else [] + add
 
 def create_patch_property(name):
     @patch(as_prop=True)
@@ -190,16 +173,77 @@ def rel(self:DuckDBPyRelation): return self._get('rel')
 
 
 @patch
-def table(self:DuckDBPyConnection, name:str, schema:str= None) -> DuckDBPyRelation:
+def table(self:DuckDBPyConnection, name:str, schema:str= None, catalog:str=None) -> DuckDBPyRelation:
     if isinstance(name, Union[List, Set, Tuple]): return [self.table(n) for n in name]
     if not isinstance(name,str): raise AttributeError
-    r = self.tables.filter(f"name == '{name}' and schema == '{schema or self.schema}'")
+    r = self.tables.filter(f"name == '{name}' and schema == '{schema or self.schema}' and catalog =='{catalog or self.catalog}'")
     catalog, schema, name, type, comment = r.fetchone()
-    tbl = self.sql(f"from {schema}.{name}")
-    tbl = tbl.set_alias(f"{schema}.{name}")
+    tbl = self.sql(f"from {catalog}.{schema}.{name}")
+    tbl = tbl.set_alias(f"{catalog}.{schema}.{name}")
     tbl._set('cls', self.dataclass(name))
     tbl._set('model', self.datamodel(name))
     meta = {'base': self, 'catalog': catalog, 'schema': schema, 'name': name, 'type': type, 'comment': comment, 'shape': tbl.shape}
     tbl._set('meta', meta)
     tbl._set('rel', tbl)
     return tbl
+
+
+
+# %% ../nbs/00_core.ipynb 34
+def noop(*args, **kwargs): return None
+def identity(x): return x
+
+
+class _Getter: 
+    """ A Getter utility check https://github.com/AnswerDotAI/fastlite """
+    def __init__(self, db:DuckDBPyConnection, type:str='', dir:List=[], get=noop): store_attr()    
+    def __dir__(self): return self.dir
+    def __str__(self): return ", ".join(dir(self))
+    def __repr__(self): return f"{str(self.db).split(' ')[-1]} {self.type.title()}s: {str(self)}"
+    def __contains__(self, s:str): return s in dir(self)
+    def __getitem__(self, k): return self.get(k)
+    def __getattr__(self, k):
+        if k[0]!='_': return self.get(k)
+        else: raise AttributeError 
+
+
+@patch
+def use(self:DuckDBPyConnection, catalog_schema:str=None, catalog:str=None, schema=None):
+    catalog, schema = catalog_schema.split('_')
+    self.sql(f"use {catalog}.{schema}")
+    print("Using ", self)
+    return self
+
+@patch(as_prop=True) # tables
+def s(self:DuckDBPyConnection): 
+    '''Autocomplete functonality for schemas'''
+    return _Getter(self, 'schema', self.tables.project(f"catalog || '_' || schema").distinct().to_list(), self.use)
+@patch(as_prop=True) # tables
+def t(self:DuckDBPyConnection): 
+    '''Autocomplete functonality for tables'''
+    return _Getter(self,'table', self.base_tables.select('name').to_list(), self.table)
+@patch(as_prop=True) # views
+def v(self:DuckDBPyConnection): 
+    '''Autocomplete functonality for views'''
+    return _Getter(self, 'view', self.views.select('name').to_list(), self.table)
+@patch(as_prop=True) # functions
+def fns(self:DuckDBPyConnection): raise NotImplementedError
+# def fns(self:DuckDBPyConnection): return _Getter(self, f"SELECT function_name FROM duckdb_functions() WHERE schema_name = '{self.schema}' and internal = False")
+
+@patch(as_prop=True) # secrets
+def shh(self:DuckDBPyConnection): raise NotImplementedError
+# def shh(self:DuckDBPyConnection): return _Getter(self, f"SELECT name FROM duckdb_secrets()")
+
+@patch
+def __repr__(self:DuckDBPyConnection): return f'{self.__class__.__name__} ({self.catalog}_{self.schema})'
+
+
+# %% ../nbs/00_core.ipynb 35
+@patch
+def _select(self:DuckDBPyRelation, k) -> DuckDBPyRelation:
+    return self.select(k) if isinstance(k, str) else self.select(*k)
+
+@patch(as_prop=True)
+def c(self:DuckDBPyRelation): 
+    '''Column autocomplete'''
+    return _Getter(self, 'column', self.columns, self._select)
